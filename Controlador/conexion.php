056cab5792fce6136fe38b5f184b6a4836d5d68a
selenarey <selena.reysosasosa@gmail.com>
2021-09-11 14:24:09 -0300

    aaaa

--- Controlador/conexion.php
+++ Controlador/conexion.php
@@ -1,2 +1,23 @@
 <?php 
- ?>
+ $bdhost = "localhost";
+ $bdusuario = "root";
+ $bdpass = "";
+ $bdnombre = "obligatorio";
+ $conectar = mysqli_connect($bdhost, $bdusuario, $bdpass, $bdnombre);
+ if (!$conectar)
+ {   
+     die("No hay conexión: ".mysqli_connect_error());
+ }
+ $doc = $_POST["txtdoc"];
+ $pass = $_POST["txtpass"];
+ $query = mysqli_query($conectar,"SELECT * FROM laboratorista WHERE documento = '".$doc."' and password = '".$pass."'");
+ $fila = mysqli_num_rows($query);
+ if($fila == 1)
+ {
+     echo "Bienvenido $doc a ";
+ }
+ else if ($fila == 0)
+ {
+     echo "<script> alert('Error');window.location='login.html'</script>";
+ }        
+?>
